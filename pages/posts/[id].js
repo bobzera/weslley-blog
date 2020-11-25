cbd0bb44ae15f1404bd79d637f2810190981e327
bobzera <weslleyzera2020@gmail.com>
2020-11-25 20:17:03 +0000

    update website the best guys

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -11,15 +11,17 @@ export default function Post({ postData }) {
             <title>{postData.title}</title>
         </Head>
         <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <a href={postData.site} target="_blank">{postData.site}</a>
+            <div className={utilStyles.lightText}>
+                <Date dateString={postData.date}/>
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
     )
   }
+
 export async function getStaticPaths() {
   const paths = getAllPostIds()
   return {
--- posts/thebestguys.co.uk.md
+++ posts/thebestguys.co.uk.md
@@ -1,9 +1,10 @@
 ---
 title: 'The Best Guys'
+site: 'https://www.thebestguys.co.uk/'
 date: '2020-11-25'
 ---
 
-**The Best Guys LTD** is a construction company in **London** created by my brother.
+**The Best Guys LTD** is a construction company in **London**.
 
 This project was recently done by me using the best technologies.
 
