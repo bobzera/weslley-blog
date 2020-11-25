c6c4d4aaf4d740d51baed37c2d29f9eff70ac686
bobzera <weslleyzera2020@gmail.com>
2020-11-25 20:33:07 +0000

    update meta tag

--- components/layout.js
+++ components/layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Hi there! Please enjoy my Blog"
         />
         <meta
           property="og:image"
