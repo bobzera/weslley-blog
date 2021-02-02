8cb56dd6cf8afeadafa2e851ad362017049b787d
bobzera <weslleyzera2020@gmail.com>
2021-02-02 19:47:25 +0000

    intall tailwindcss

--- components/btn.js
+++
@@ -1,9 +0,0 @@
-import style from './btn.module.css'
-
-export default function Btn({children}){
- return (    
-        <div className={style.border}>        
-            {children}
-        </div>   
- )
-}
