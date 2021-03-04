f78a0fac81dfb18ef347a3cc1a5a081855a84bff
bobzera <weslleyzera2020@gmail.com>
2021-03-04 23:11:56 +0000

    button link github

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Nav from '../components/nav';
+import Link from 'next/link'
 
 export default function Home(){
     return(
@@ -9,7 +10,9 @@ export default function Home(){
                     <h1 className="text-3xl font-bold text-gray-title">Desenvolvo projetos incríveis <span className="text-green ">.</span></h1>
                     <h2 className="text-gray-text py-4 leading-relaxed">Aplicações fullstack para web e mobile desde o layout até o código</h2>
                     <img className="py-16" src="https://josepholiveira.dev/_next/static/images/presentation-programmer-c7fa23b9eb3a27b094a594ff5a5ad7e5.svg"/>
-                    <button className="px-16 rounded mx-auto py-4 bg-purple text-white w-full">Iniciar um projeto</button>
+                    <Link href="https://github.com/weslley-oliveira">
+                        <button className="px-16 rounded mx-auto py-4 bg-purple text-white w-full border-b-4 border-purple-600">Iniciar um projeto</button>
+                    </Link>
                 </div>
                 
             </div>
