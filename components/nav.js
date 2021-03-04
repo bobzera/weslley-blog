69c69b62ca6f757fce442dd635e01e50ba85226f
bobzera <weslleyzera2020@gmail.com>
2021-03-04 23:05:01 +0000

    favicon

--- components/nav.js
+++ components/nav.js
@@ -2,7 +2,7 @@ export default function nav(){
     return(
         <div className="flex justify-between pt-6">
             <h1 className="text-gray-title font-semibold tracking-wide">              
-                <span className="text-purple text-2xl  font-bold p-1"><i class="fa fa-code" aria-hidden="true"></i></span>
+                <span className="text-purple text-2xl  font-bold p-1"><i className="fa fa-code" aria-hidden="true"></i></span>
                 WeslleyOliveira
             </h1>
             <span className="text-purple text-2xl font-light">
